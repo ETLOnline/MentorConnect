1c09f9afdc1415a7c537ecdf2f2e0fa2b262e320
AteeqAkbar <56605324+AteeqAkbar@users.noreply.github.com>
2022-11-30 18:53:56 +0500

    config firebase and get user data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import HomePage from "../components/homePage";
 import styles from "../styles/Home.module.css";
 import { userProxy } from "../apiProxy/userProxy";
+import { getUsers } from "../utils_firebase/users";
 export default function Home() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    function name() {
+      const users = getUsers();
+      setUser(users);
+    }
+    name();
+  }, []);
+
+  if (!user) {
+    return <p>loading</p>;
+  }
   let data = new userProxy();
 
   // console.log(data.getFeaturedMentors());
@@ -17,7 +30,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <HomePage />
+      <HomePage mentor={user} />
     </Fragment>
   );
 }
