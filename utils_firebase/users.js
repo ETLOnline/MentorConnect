10ba37f1cb1b31a11092f33912e922a7adaac8d0
Muhammad sajid <101540717+Muhammadsajid12@users.noreply.github.com>
2022-12-01 15:52:56 +0500

    display data from firebase

--- utils_firebase/users.js
+++ utils_firebase/users.js
@@ -1,16 +1,11 @@
 import { fireStore } from "./config";
-export const getUsers = () => {
+
+export const getUsers = async () => {
   const resualt = [];
-  fireStore
-    .collection("users")
-    // .doc("HID06ysBc8cYx2rtsxvOAtOJT9o1")
-    // .collection("details")
-    .get()
-    .then(async (snapshot) => {
-      // console.log(snapshot.docs);
-      snapshot.docs.forEach((doc) => {
-        resualt.push({ id: doc.id, ...doc.data() });
-      });
-    });
+  const data = await fireStore.collection("users").get();
+
+  data.docs.forEach((doc) => {
+    resualt.push({ id: doc.id, ...doc.data() });
+  });
   return resualt;
 };
