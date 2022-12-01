8fbe3e5269b86567e970d16417f7bd2adc6e6766
Muhammad sajid <101540717+Muhammadsajid12@users.noreply.github.com>
2022-12-01 17:55:11 +0500

    Update signupPage

--- components/homePage/card.js
+++ components/homePage/card.js
@@ -13,7 +13,7 @@ const Card = ({ mentors }) => {
             return (
               <Fragment key={mentor.id}>
                 <div id="featuredProducts" className=" mb-8 m-2 w-100">
-                  <div className=" px-2 py-11 w-full  ">
+                  <div className=" px-2 py-11  w-full  ">
                     <div className="relative group rounded-[12px]">
                       <div className="h-[352px] rounded-[12px]">
                         <img
@@ -25,7 +25,7 @@ const Card = ({ mentors }) => {
 
                       <div className="absolute w-full rounded-[12px] top-0 right-0 bottom-0 left-0 h-full overflow-hidden bg-fixed bg-gradient-to-t from-[rgba(0,0,0,0.9)] to-[rgba(36,36,36,0.3)] rounded-xl opacity-50 "></div>
                       <div className="flex flex-col w-[90%] ml-4 absolute group-hover:-translate-y-10 group-hover:duration-1000 transition-all duration-1000 top-[286px]  transition-all hover:duration-1000">
-                        <div className="flex justify-between mt-1 ">
+                        <div className="flex justify-between mt-10 ">
                           <p className="text-[20px] text-[#FFFFFF] leading-[23px] font-normal font-['Raleway']">
                             {mentor.summry.displayName}
                           </p>
