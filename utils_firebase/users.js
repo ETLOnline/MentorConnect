eb743588682a8e7e87fb40946b1676969f0d0494
AteeqAkbar <56605324+AteeqAkbar@users.noreply.github.com>
2022-12-05 17:57:42 +0500

    user sign up and login  with email and password

--- utils_firebase/users.js
+++ utils_firebase/users.js
@@ -9,22 +9,42 @@ export const getUsers = async () => {
   });
   return resualt;
 };
-export const LoginWithEmailPassword = () => {
+
+// Sign Up create user
+export const SignupWithEmailPassword = (data) => {
   console.log(
+    data,
     ">>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
   );
+  const image = data.image
+    ? data.image
+    : "https://www.iconpacks.net/icons/1/free-user-icon-295-thumb.png";
   auth
-    .createUserWithEmailAndPassword("15aliraza786@gmail.com", "password")
+    .createUserWithEmailAndPassword(data.gmail, data.password)
     .then((userCredential) => {
       console.log(userCredential, "create user");
       userCredential.user.sendEmailVerification().then(() => {
         // Email verification sent!
-        // ...
+        fireStore
+          .collection("users")
+          .doc(userCredential.user.uid)
+          .set({
+            uid: userCredential.user.uid,
+            summry: {
+              displayName: data.firstName + data.lastName,
+              email: data.gmail,
+              image: image,
+            },
+          })
+          .then(() => {
+            console.log("Document successfully written!");
+          });
 
         console.log("Email send");
       });
       // Signed in
       var user = userCredential.user;
+      console.log(user, "Signed in");
       // ...
     })
     .catch((error) => {
@@ -32,5 +52,24 @@ export const LoginWithEmailPassword = () => {
       var errorCode = error.code;
       var errorMessage = error.message;
       // ..
+    });
+};
+
+// login with email password
+
+export const LoginWithEmailPassword = (data) => {
+  auth
+    .signInWithEmailAndPassword(data.gmail, data.password)
+    .then((userCredential) => {
+      // Signed in
+
+      var user = userCredential.user;
+      console.log(user, "login");
+      // ...
+    })
+    .catch((error) => {
+      console.log(error);
+      var errorCode = error.code;
+      var errorMessage = error.message;
     });
 };
