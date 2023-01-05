e1c7df3bf6645a19e311f83eb8610b477444ee6b
Muhammad sajid <101540717+Muhammadsajid12@users.noreply.github.com>
2023-01-05 15:38:19 +0500

    useStatechange value

--- pages/home.js
+++ pages/home.js
@@ -7,28 +7,14 @@ import { AuthContext } from "../contexts/auth_context";
 import Spinner from "../components/spinner";
 import UpCommingSection from "../components/homePage/upCommingSection";
 import { getSessionByUserSkills } from "../utils_firebase/sessions";
-import { useRouter } from "next/router";
 import InterestSkills from "../components/tiles/interestSkills";
 import RecomendedSkills from "../components/tiles/recomendedSkills";
 
 const Home = () => {
   const { user } = useContext(AuthContext);
-  // const [Interests, setInterest] = useState(null);
   const [RecommendedSession, setRecommendedSession] = useState();
-  const [auth, setAuth] = useState();
-  const router = useRouter();
-  // console.log(user.user.uid, "homeeeeeeeeeeeeeeeeee");
-
   console.log(user, "userHome");
 
-  // if (user.authIsValide) {
-  //   // router.push("/");
-  //   return (
-  //     <div className=" text-center  items-center  text-5xl pb-20 pt-20">
-  //       <p> Firts login please</p>
-  //     </div>
-  //   );
-  // }
   const generalSkills = ["HTML", "CSS"];
   console.log(user.user, "userHome...");
 
@@ -52,7 +38,7 @@ const Home = () => {
   if (!RecommendedSession) {
     return (
       <div className=" text-center  items-center  text-5xl pb-20 pt-20">
-        <p> First login please</p>
+        <p>Loading.... </p>
       </div>
     );
   }
