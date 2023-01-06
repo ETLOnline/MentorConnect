270cc371da1fb6560a3f84870b8b5faecf8b1ee3
Arslan591 <Arslanmughaldev@gmail.com>
2023-01-06 17:01:47 +0500

    changes

---
+++ components/tiles/profileContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const profileContent = () => {
+  return (
+    <>
+      <div>
+        <button>About</button>
+        <button>Activity</button>
+        <button>Rewards</button>
+        <button>MyShedule</button>
+      </div>
+    </>
+  );
+};
+export default profileContent;
--- pages/home.js
+++ pages/home.js
@@ -2,9 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 
 import MyMentor from "../components/tiles/myMentor";
 import RegisteredSession from "../components/tiles/registeredSession";
-import { getSingleUser } from "../utils_firebase/users";
 import { AuthContext } from "../contexts/auth_context";
-import Spinner from "../components/spinner";
 import UpCommingSection from "../components/homePage/upCommingSection";
 import { getSessionByUserSkills } from "../utils_firebase/sessions";
 import InterestSkills from "../components/tiles/interestSkills";
@@ -13,35 +11,25 @@ import RecomendedSkills from "../components/tiles/recomendedSkills";
 const Home = () => {
   const { user } = useContext(AuthContext);
   const [RecommendedSession, setRecommendedSession] = useState();
-  console.log(user, "userHome");
 
   const generalSkills = ["HTML", "CSS"];
   console.log(user.user, "userHome...");
+  if (!user.user) {
+    return (
+      <div className=" text-center  items-center  text-5xl pb-20 pt-20">
+        <p>First Login Please</p>
+        <p>Loading.... </p>
+      </div>
+    );
+  }
 
   useEffect(() => {
-    // if (!user.authIsValide) {
-    //   // router.push("/");
-    //   return (
-    //     <div className=" text-center  items-center  text-5xl pb-20 pt-20">
-    //       <p> Firts login please</p>
-    //     </div>
-    //   );
-    // }
-
     getSessionByUserSkills(
       user.user.interest.length === 0 ? generalSkills : user.user.interest
     ).then((data) => {
       setRecommendedSession(data);
     });
   }, []);
-
-  if (!RecommendedSession) {
-    return (
-      <div className=" text-center  items-center  text-5xl pb-20 pt-20">
-        <p>Loading.... </p>
-      </div>
-    );
-  }
 
   console.log(RecommendedSession, "RecommendedSession....");
   return (
