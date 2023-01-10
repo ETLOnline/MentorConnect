c5be055a66dd9ac92fdbf7be329b3c5f11d3f28f
AteeqAkbar <56605324+AteeqAkbar@users.noreply.github.com>
2023-01-10 19:40:52 +0500

    remove error

--- pages/auth/[userId].js
+++ pages/auth/[userId].js
@@ -9,11 +9,6 @@ import { getSingleUser } from "../../utils_firebase/users";
 import { AuthContext } from "../../contexts/auth_context";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import Loader from "../../components/tiles/loader";
-
-import SmIntoCard from "../../components/tiles/smIntroCard";
-import Avatar from "../../components/tiles/avatar";
-
 const Index = () => {
   const { user } = useContext(AuthContext);
   const [User, setUser] = useState();
@@ -38,6 +33,7 @@ const Index = () => {
   }, [path, follower]);
   // Taking data from child component to rerender the component to increase follower😎
   function Rerender(data) {
+    console.log(data, "data is from child");
     setFollower(data);
   }
 
@@ -82,9 +78,8 @@ const Index = () => {
   return (
     <>
       <div className="relative">
-        <div className="relative min-w-full h-[350px] ">
+        <div className="min-w-full h-[350px] ">
           <Image src="/img/Frame 83.png" alt="img" fill />
-          <Avatar />
         </div>
 
         <div className="invisible md:visible fixed bottom-[7%] w-[35.56%] max-w-[512px]">
@@ -101,53 +96,58 @@ const Index = () => {
                 className="w-[19.44%] py-0 px-[5.55%] focus:border-b-[2px] focus:border-[#1C2D56]  h-[73.56%] flex items-center text-[24px] focus:text-[#1C2D56] font-normal leading-[28px] font-['Raleway'] text-center"
                 onClick={abouthandler}
               >
-                
                 About
               </button>
-          <div className="w-full md:h-[87px] h-[183px]  flex border-b-[1px] border-[#1C2D56]">
-            <div className=" md:w-[37.20%]"></div>
-            <div className=" md:flex flex-wrap justify-between h-[87px] w-full md:w-[62.8%] items-end">
-              <div className="  visible md:hidden ml-[37.20%] ">
-                <SmIntoCard />
-              </div>
-              <div className="flex flex-wrap justify-between w-[100%] items-end">
-                <button
-                  className="w-[19.44%] py-0 px-[5.55%] focus:border-b-[2px] focus:border-[#1C2D56]  h-[73.56%] flex items-center text-[2.8vw] md:text-[24px] focus:text-[#1C2D56] font-normal leading-[28px] font-['Raleway'] text-center"
-                  onClick={abouthandler}
-                >
-                  About
-                </button>
 
-                <button
-                  className="w-[19.44%] focus:border-b-[2px] py-0 px-[4.51%] focus:border-[#1C2D56]  h-[73.56%] flex items-center text-[2.8vw] md:text-[24px] focus:text-[#1C2D56] font-normal leading-[28px] font-['Raleway']"
-                  onClick={activityhandler}
-                >
-                  Activity
-                </button>
+              <button
+                className="w-[19.44%] focus:border-b-[2px] py-0 px-[4.51%] focus:border-[#1C2D56]  h-[73.56%] flex items-center text-[24px] focus:text-[#1C2D56] font-normal leading-[28px] font-['Raleway']"
+                onClick={activityhandler}
+              >
+                Activity
+              </button>
 
-                <button
-                  className="w-[19.44%] py-0 px-[4.51%x] focus:border-b-[2px] focus:border-[#1C2D56]  h-[73.56%] flex items-center text-[2.8vw] md:text-[24px] focus:text-[#1C2D56] font-normal leading-[28px] font-['Raleway']"
-                  onClick={rewardhandler}
-                >
-                  Rewards
-                </button>
-                <button
-                  className="w-[19.44%] py-0  focus:border-b-[2px] focus:border-[#1C2D56]  h-[73.56%] flex items-center text-[2.8vw] md:text-[24px] focus:text-[#1C2D56] font-normal leading-[28px] font-['Raleway']"
-                  onClick={schdulehandler}
-                >
-                  My Schdule
-                </button>
-              </div>
+              <button
+                className="w-[19.44%] py-0 px-[4.51%x] focus:border-b-[2px] focus:border-[#1C2D56]  h-[73.56%] flex items-center text-[24px] focus:text-[#1C2D56] font-normal leading-[28px] font-['Raleway']"
+                onClick={rewardhandler}
+              >
+                Rewards
+              </button>
+              <button
+                className="w-[19.44%] py-0  focus:border-b-[2px] focus:border-[#1C2D56]  h-[73.56%] flex items-center text-[24px] focus:text-[#1C2D56] font-normal leading-[28px] font-['Raleway']"
+                onClick={schdulehandler}
+              >
+                My Schdule
+              </button>
             </div>
           </div>
 
           <div className="w-full flex">
             <div className="w-[37.20%]"></div>
-            <div className="md:w-[62.8%]">
+            <div className="w-[62.8%]">
               {about && <About data={User} />}
               {activity && <Activity />}
               {reward && <Rewards />}
               {schdule && <Schdule />}
+              {/* <br></br>
+              <IntrestsTile />
+              <br></br>
+              <LearningTile />
+              <br></br>
+              <BadgesCircle />
+              <br></br>
+              <RecomendationCard />
+              <br></br>
+              <LearningCard />
+              <br></br>
+              <CoachingCard />
+              <br></br>
+              <ReviewsCard />
+              <br></br>
+              <FeedbackCard />
+              <br></br>
+              <SkillRating />
+              <br></br>
+              <SkillRating2 /> */}
             </div>
           </div>
         </div>
