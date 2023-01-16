6f8843f32d1efbff645e1bdc167a41a464b93591
AteeqAkbar <56605324+AteeqAkbar@users.noreply.github.com>
2023-01-16 13:01:01 +0500

    on click skill show session

--- components/tiles/interestSkillsCard.js
+++ components/tiles/interestSkillsCard.js
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import Image from "next/image";
 
 const InterestSkillsCard = ({ skill, click }) => {
   const [selectSkill, setSelectSkill] = useState(false);
-
+  useEffect(() => {
+    if (click) {
+      click(skill.name, selectSkill);
+    }
+  }, [selectSkill]);
   return (
     <div
       onClick={() => {
         setSelectSkill((prev) => !prev);
-        click(skill.name, !selectSkill);
+        // if (click) {
+        //   click(skill.name, !selectSkill);
+        // }
       }}
-      className={
-        selectSkill
-          ? "cursor-pointer font-bold scale-105 shadow-2xl rounded-[12px] bg-[rgba(157,213,200,0.14)]"
-          : "cursor-pointer"
-      }
     >
-      <div className="min-w-[21.53%] mb-[15px] rounded-[12px] flex items-center  shadow-md gap-2 px-2">
+      <div
+        className={
+          selectSkill
+            ? "min-w-[21.53%] mb-[15px] rounded-[12px] flex items-center shadow-md gap-2 px-2 cursor-pointer scale-105 shadow-2xl bg-[rgba(157,213,200,0.14)]"
+            : "min-w-[21.53%] mb-[15px] rounded-[12px] flex items-center shadow-md gap-2 px-2 cursor-pointer"
+        }
+      >
         <div className="flex w-[42px] h-[42px] m-2 ">
           <Image
             src={skill.image}
