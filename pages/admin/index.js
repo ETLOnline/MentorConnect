b66a11c35813284aa376884f2afb4e2f37d81e42
15AliRaza <gamescomplete786@gmail.com>
2023-01-19 15:01:36 +0500

    session table admin side

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -4,25 +4,54 @@ import Header from "../../components/admin/header";
 import Table from "../../components/admin/table";
 import Spinner from "../../components/spinner";
 import { getUsers } from "../../utils_firebase/users";
+import { getAllSessions } from "../../utils_firebase/sessions";
+import SessionTable from "../../components/admin/sessionTable";
 
 function Index() {
   const [userData, setUserData] = useState([]);
+  const [sessionData, setSessionData] = useState([]);
+  const [userTable, setUserTable] = useState(true);
+  const [sessionTable, setSessionTable] = useState(false);
   useEffect(() => {
     getUsers().then((data) => {
-      // console.log(data, "admin");
+      console.log(data, "admin");
       setUserData(data);
     });
+    getAllSessions().then((sdata) => {
+      console.log(sdata, "session data");
+      setSessionData(sdata);
+    });
   }, []);
+  const userTableHandler = () => {
+    console.log("user table hazir ho");
+    setUserTable(true);
+    setSessionTable(false);
+  };
+  const sessionTableHandler = () => {
+    console.log("session table hazir ho");
+    setUserTable(false);
+    setSessionTable(true);
+  };
 
   return (
     <div>
       <div className="relative bg-blue-50 overflow-hidden max-h-full">
         <Header />
-        <Aside />
+        <Aside
+          userTable={userTableHandler}
+          sessionTable={sessionTableHandler}
+        />
 
         <main className="ml-60 pt-16 ">
           <div className="px-6 bg-white py-8">
-            {userData.length > 0 ? <Table users={userData} /> : <Spinner />}
+            {userTable &&
+              (userData.length > 0 ? <Table users={userData} /> : <Spinner />)}
+            {sessionTable &&
+              (sessionData.length > 0 ? (
+                <SessionTable sessions={sessionData} />
+              ) : (
+                <Spinner />
+              ))}
           </div>
         </main>
       </div>
