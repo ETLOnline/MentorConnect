391e2ef130acaac47909db89fed36c0fbc1dfbcd
15AliRaza <gamescomplete786@gmail.com>
2023-04-18 14:26:26 +0500

    again

--- utils_firebase/config.js
+++ utils_firebase/config.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "development") {
     // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     apiKey: "AIzaSyDLTVghMhpGtXg9laD2Q9vxYLSE4PnZUdk",
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    authDomain: "tldweb-dev.firebaseapp.com",
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
